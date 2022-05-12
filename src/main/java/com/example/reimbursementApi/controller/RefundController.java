8287216c10f0d5fc038c604c7496181bf93c79de
keoffor <kenobago@gmail.com>
2022-05-11 20:49:04 -0500

    added artifacts

--- src/main/java/com/example/reimbursementApi/controller/RefundController.java
+++ src/main/java/com/example/reimbursementApi/controller/RefundController.java
@@ -17,7 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/reimbursement")
 public class RefundController {
-    @Value("${api.config.api2-url: EMAIL_URL}")
+    @Value("${api.config.api2-url:}")
     String email_url;
 
     @Autowired
